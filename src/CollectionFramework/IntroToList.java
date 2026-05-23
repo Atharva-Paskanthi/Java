91e87cf9c17b6063ec3cd3f82ee71b4bf1d05fc1
Atharva Paskanthi <atharva111lp@gmail.com>
2026-05-23 17:28:21 +0530

    Collection Framework

--- src/CollectionFramework/IntroToList.java
+++ src/CollectionFramework/IntroToList.java
@@ -13,6 +13,13 @@ public class IntroToList {
         list1.add(true);
         list1.add(925892834589.9346);
 
+        String getStringValue = list1.get(0).toString(); //toString converts the value into string
+        System.out.println("Print Value : "+getStringValue);
+
+        Integer getIntValue = (Integer) list1.get(1); //(Integer) convert the value into integer while retrieving
+        System.out.println("Print Value : "+getIntValue);
+
+
         List<Integer> list = new ArrayList<>();
 
     }
