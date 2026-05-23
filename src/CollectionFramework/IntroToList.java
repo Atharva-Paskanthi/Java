d252d2a3ca7d02ad3eb3afb158e55e81edd5081d
Atharva Paskanthi <atharva111lp@gmail.com>
2026-05-23 16:57:53 +0530

    Collection Framework started

---
+++ src/CollectionFramework/IntroToList.java
@@ -0,0 +1,19 @@
+package CollectionFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroToList {
+
+    public static void main(String[] args) {
+
+        List list1 = new ArrayList<>();
+        list1.add("Java");
+        list1.add(123);
+        list1.add(true);
+        list1.add(925892834589.9346);
+
+        List<Integer> list = new ArrayList<>();
+
+    }
+}
