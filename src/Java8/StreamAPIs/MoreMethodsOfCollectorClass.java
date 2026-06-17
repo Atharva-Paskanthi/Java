a6e8faa7ecb3642d90a4d22724b11cfaca9a5f96
Atharva Paskanthi <atharva111lp@gmail.com>
2026-06-17 10:48:55 +0530

    Stream - Collectors method

--- src/Java8/StreamAPIs/MoreMethodsOfCollectorClass.java
+++ src/Java8/StreamAPIs/MoreMethodsOfCollectorClass.java
@@ -11,11 +11,35 @@ public class MoreMethodsOfCollectorClass {
 
         List<String> cities = Arrays.asList("Mumbai","Delhi","Bangalore","Chennai");
 
-        List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+        //Joining elements into single String
+        String joinedCities = cities.stream().
+                collect(Collectors.joining("**"));
+        System.out.println("Joined String : "+joinedCities);
+
+        String joinedCities1 = cities.stream().
+                collect(Collectors.joining()); //Without Delimiter
+        System.out.println("Joined String : "+joinedCities1);
+
+        System.out.println("---------------------------------------------------");
+
+        List<Integer> intList  = Arrays.asList(10,20,30,40,50);
+        String joinedInt = intList.stream().
+                map(String::valueOf).
+                collect(Collectors.joining("->"));
+        System.out.println(joinedInt);
+
+        System.out.println("---------------------------------------------------");
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,10);
+
+        //Summing all integers in the list
+        int sum = numbers.stream().collect(Collectors.summingInt(s -> s));
+        System.out.println("Sum of all integers : "+sum);
+
+        System.out.println("---------------------------------------------------");
 
         List<String> names = Arrays.asList("Amit","Deepa","Rahul","Nikhil","Rishi","Rohit","Java");
 
-        System.out.println("---------------------------------------------------");
         //Grouping names by First letter of name
         Map<Character, List<String>> groupedByLetter = names.stream().
                         collect(Collectors.groupingBy(name -> name.charAt(0)));
